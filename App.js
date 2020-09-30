adeb71ea3679ba29d438592ad2a28fa5ec7c22b6
Mike <troianello@protonmail.com>
2020-09-29 22:31:38 -0500

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
+
+import Main from './components/MainComponent';
+
+const { persistor, store } = ConfigureStore();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Main />
+      </PersistGate>
+    </Provider>
   );
 }
 
